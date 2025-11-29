f334a36558d9a4cdf6b70566c90f5d4dba783a32
ASh <ashank13mishra@gmail.com>
2025-11-30 04:49:20 +0530

    Fix CORS and Cookie settings for production

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -87,7 +87,7 @@ export const callback = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
